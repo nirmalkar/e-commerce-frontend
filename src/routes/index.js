2f2ba90cd1a8baa6c64b247cfb6f963c42009f85
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-06-21 00:53:16 +0530

    ROUTES: route added for cart

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import ProductDetails from "../pages/ProductDetails";
+import Cart from "../pages/Cart";
 
 import Home from "../pages/Home";
 
@@ -8,6 +9,7 @@ const Routes = () => {
   return (
     <>
       <Route path="/products/:id" component={ProductDetails} />
+      <Route path="/cart/:id?" component={Cart} />
       <Route exact path="/" render={() => <Home />} />
     </>
   );
