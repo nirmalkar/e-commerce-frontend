525640ee1c74a2b9fa6cb22ac73a2857e8ef8ace
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-06-27 23:48:45 +0530

    ROUTES_CREATED: shipping,payment,and placeorder

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,13 +7,19 @@ import Home from "../pages/Home";
 import SignIn from "../pages/SignIn";
 import Register from "../pages/Register";
 import AddProduct from "../pages/AddProduct";
+import Shipping from "../pages/Shipping";
+import Payment from "../pages/Payment";
+import PlaceOrder from "../pages/PlaceOrder";
 
 const Routes = () => {
   return (
     <>
       <Route path="/products/:id" component={ProductDetails} />
       <Route path="/cart/:id?" component={Cart} />
+      <Route path="/shipping" component={Shipping} />
       <Route exact path="/signin" component={SignIn} />
+      <Route exact path="/payment" component={Payment} />
+      <Route exact path="/placeorder" component={PlaceOrder} />
       <Route exact path="/register" component={Register} />
       <Route exact path="/add-product" component={AddProduct} />
       <Route exact path="/" render={() => <Home />} />
