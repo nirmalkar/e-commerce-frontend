86a81b76fa77a24e9e2acc31eff955d0f914d765
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-07-01 13:42:10 +0530

    LANDING: landing page added

---
+++ src/pages/Landing.js
@@ -0,0 +1,16 @@
+import React from "react";
+import LandingMainHero from "../components/Landing/LandingMainHero";
+import Footer from "../components/Landing/Footer";
+import LandingSecondHero from "../components/Landing/LandingSecondHero";
+
+const Landing = () => {
+  return (
+    <>
+      <LandingMainHero />
+      <LandingSecondHero />
+      <Footer />
+    </>
+  );
+};
+
+export default Landing;
