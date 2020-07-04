551f3e8c0493a311e0a456fd0658c6fef992566e
hemantkumar2 <hemantnirmalkar17@gmail.com>
2020-07-05 00:56:44 +0530

    COMPONENT-SEARCH_BAR: search action dispatched

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -1,12 +1,35 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import { searchProduct } from "../../appRedux/action/searchAction";
+import { useInputState } from "../../hooks/useInputState";
 
 const SearchBar = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const [search, setSearch] = useInputState("");
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(searchProduct(search));
+    history.push(`/products/`);
+  };
   return (
-    <div className="field">
-      <div className="control w-6 is-loading">
-        <input className="input is-rounded" type="text" placeholder="Search" />
+    <form onSubmit={submitHandler}>
+      <div className="field">
+        <div className="control w-24 is-loading has-icons-left">
+          <input
+            className="input is-rounded"
+            type="text"
+            placeholder="Search"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <span className="icon is-small is-left">
+            <i className="fas fa-search" />
+          </span>
+        </div>
       </div>
-    </div>
+    </form>
   );
 };
 
